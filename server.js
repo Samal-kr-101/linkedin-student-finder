1ba5abf8bfdf607b45aca69b46fdf96cb7709bef
Samal Kumar <samalkr99054@gmail.com>
2026-05-12 16:18:45 +0530

    LinkedIn scraper working version

--- server.js
+++ server.js
@@ -1,32 +1,3 @@
-// const express = require("express");
-// const scrapeLinkedIn = require("./scraper");
-
-// const app = express();
-
-// app.get("/scrape", async (req, res) => {
-//   try {
-//     const data = await scrapeLinkedIn();
-
-//     res.json({
-//       success: true,
-//       profiles: data,
-//     });
-//   } catch (error) {
-//     console.log(error);
-
-//     res.status(500).json({
-//       success: false,
-//       error: error.message,
-//     });
-//   }
-// });
-
-// app.listen(5000, () => {
-//   console.log("Server running on port 5000");
-// });
-
-
-
 const express = require("express");
 const cors = require("cors");
 
